c359a8dee0777afb60e658be9a1b0308f4ed9f56
smitasahu2 <smitasahuiitd@gmail.com>
2023-10-13 14:45:44 +0100

    added october tests

--- Smita_model/Test_August/DFN_model/DFN_only.py
+++ Smita_model/Test_August/DFN_model/DFN_only.py
@@ -1,7 +1,4 @@
-#
 # Example showing how to load and solve the DFN
-#
-
 import pybamm
 import numpy as np
 
@@ -9,9 +6,6 @@ pybamm.set_logging_level("INFO")
 
 # load model
 model_DFN = pybamm.lithium_ion.DFN()
-# create geometry
-# geometry = model.default_geometry
-
 # load parameter values and process model and geometry
 parameter_values = pybamm.ParameterValues("OKane2022")
 parameter_values["Current function [A]"] = 20
@@ -26,11 +20,8 @@ var_pts = {
 t_eval = np.linspace(0, 3600, 10000)
 # solver = pybamm.CasadiSolver(mode="safe", atol=1e-6, rtol=1e-3)
 # solution = solver.solve(model, t_eval)
-
 sim_DFN = pybamm.Simulation(model_DFN, parameter_values=parameter_values, var_pts=var_pts)
 sol_DFN = sim_DFN.solve(t_eval)
-
-
 # plot
 plot = pybamm.QuickPlot(
     sol_DFN,
@@ -49,9 +40,16 @@ plot = pybamm.QuickPlot(
 )
 plot.dynamic_plot()
 
-sol_DFN.save_data("DFN.csv",[
-                        "Time [s]", 
+sol_DFN.save_data("/home/smita/workspace/comparison/PyBaMM/Smita_model/Test_Sept/DFN_model/DFN.csv",[
+        "Time [s]", 
         "Current [A]",
-         "Voltage [V]",
+        "Voltage [V]",
+        "Negative particle concentration [mol.m-3]",
+        "Negative electrode potential [V]",
+        "Positive particle concentration [mol.m-3]",
+        "Positive electrode potential [V]",
+        "Electrolyte concentration [mol.m-3]",
+        "Electrolyte potential [V]",
+        
   ]                      
   , to_format="csv")
--- Smita_model/Test_Sept/DFN_model/DFN_only.py
+++ Smita_model/Test_Sept/DFN_model/DFN_only.py
@@ -1,20 +1,18 @@
-#
 # Example showing how to load and solve the DFN
-#
-
 import pybamm
 import numpy as np
-
 pybamm.set_logging_level("INFO")
 
-# load model
-model_DFN = pybamm.lithium_ion.DFN()
-# create geometry
-# geometry = model.default_geometry
 
-# load parameter values and process model and geometry
-parameter_values = pybamm.ParameterValues("OKane2022")
-parameter_values["Current function [A]"] = 20
+"""""
+test1 = 1  # param["Current function [A]"] = 5/4, C/4 c-rate
+test2 = 2  # param["Current function [A]"] = 5/2, C/2 c-rate
+test3 = 3  # param["Current function [A]"] = 5, pybamm's default value 1C rate
+test4 = 4  # param["Current function [A]"] = 1.5*5, 1.5C c-rate
+test4 = 5  # param["Current function [A]"] = 2*5, 2C c-rate
+"""""
+params = pybamm.ParameterValues("OKane2022")
+model_DFN = pybamm.lithium_ion.DFN()
 var_pts = {
     "x_n": 30,  # negative electrode
     "x_s": 15,  # separator 
@@ -22,36 +20,59 @@ var_pts = {
     "r_n": 30,  # negative particle
     "r_p": 30,  # positive particle
  }
-# solve model
-t_eval = np.linspace(0, 3600, 10000)
-# solver = pybamm.CasadiSolver(mode="safe", atol=1e-6, rtol=1e-3)
-# solution = solver.solve(model, t_eval)
+testall = [1]#, 2, 3, 4, 5]
 
-sim_DFN = pybamm.Simulation(model_DFN, parameter_values=parameter_values, var_pts=var_pts)
-sol_DFN = sim_DFN.solve(t_eval)
+for test in testall:
+    if test == 1:
+        params["Current function [A]"] = 5/4
+        print("Current function 5/4 [A]: C/4 c-rate")
+        t_eval = np.linspace(0, 3600*4, 10000)
+        sim_DFN = pybamm.Simulation(model_DFN, parameter_values=params, var_pts=var_pts)
+        sol_DFN = sim_DFN.solve(t_eval)
+        plot = pybamm.QuickPlot(sol_DFN)
+        sol_DFN.save_data("DFN_C_by4.csv",["Time [s]", "Current [A]", 
+                    "Voltage [V]",], to_format="csv")
+        plot.dynamic_plot()
+    elif test == 2:
+        params["Current function [A]"] = 5/2
+        print("Current function 5/2 [A]: C/2 c-rate")
+        t_eval = np.linspace(0, 3600*2, 10000)
+        sim_DFN = pybamm.Simulation(model_DFN, parameter_values=params, var_pts=var_pts)
+        sol_DFN = sim_DFN.solve(t_eval)
+        plot = pybamm.QuickPlot(sol_DFN)
+        sol_DFN.save_data("DFN_C_by2.csv",["Time [s]", "Current [A]", 
+                    "Voltage [V]",], to_format="csv")
+        plot.dynamic_plot() 
+    if test == 3:
+        params["Current function [A]"] = 5
+        print("Current function 5 [A]: 1C c-rate")
+        t_eval = np.linspace(0, 3600, 10000)
+        sim_DFN = pybamm.Simulation(model_DFN, parameter_values=params, var_pts=var_pts)
+        sol_DFN = sim_DFN.solve(t_eval)
+        plot = pybamm.QuickPlot(sol_DFN)
+        sol_DFN.save_data("DFN_1C.csv",["Time [s]", "Current [A]", 
+                    "Voltage [V]",], to_format="csv")
+        plot.dynamic_plot()
+    if test == 4:
+        params["Current function [A]"] = 5*1.5
+        print("Current function 5*1.5 [A]: 1.5C c-rate")
+        t_eval = np.linspace(0, 3600/1.5, 10000)
+        sim_DFN = pybamm.Simulation(model_DFN, parameter_values=params, var_pts=var_pts)
+        sol_DFN = sim_DFN.solve(t_eval)
+        plot = pybamm.QuickPlot(sol_DFN)
+        sol_DFN.save_data("DFN_C_by3by2.csv",["Time [s]", "Current [A]", 
+                    "Voltage [V]",], to_format="csv")
+        plot.dynamic_plot()
+    if test == 5:
+        params["Current function [A]"] = 5*2
+        print("Current function 5*2 [A]: 2C c-rate")
+        t_eval = np.linspace(0, 3600/2, 10000)
+        sim_DFN = pybamm.Simulation(model_DFN, parameter_values=params, var_pts=var_pts)
+        sol_DFN = sim_DFN.solve(t_eval)
+        plot = pybamm.QuickPlot(sol_DFN)
+        sol_DFN.save_data("DFN_2C.csv",["Time [s]", "Current [A]", 
+                    "Voltage [V]",], to_format="csv")
+        plot.dynamic_plot()               
 
 
-# plot
-plot = pybamm.QuickPlot(
-    sol_DFN,
-    [
-        "Negative particle concentration [mol.m-3]",
-        "Electrolyte concentration [mol.m-3]",
-        "Positive particle concentration [mol.m-3]",
-        "Current [A]",
-        "Negative electrode potential [V]",
-        "Electrolyte potential [V]",
-        "Positive electrode potential [V]",
-        "Voltage [V]",
-    ],
-    time_unit="seconds",
-    spatial_unit="um",
-)
-plot.dynamic_plot()
-
-sol_DFN.save_data("DFN.csv",[
-                        "Time [s]", 
-        "Current [A]",
-         "Voltage [V]",
-  ]                      
-  , to_format="csv")
+  
